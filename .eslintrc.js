81d2af260aa34955e592dcb13c125aba1dc86fe7
Timo Clasen <goebeltimo@gmail.com>
2021-03-13 18:42:50 +0100

    Add ESlint Import Sort Plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,16 +25,17 @@ module.exports = {
   ],
   rules: {
     "prettier/prettier": ["error", {}, { usePrettierrc: true }], // Use our .prettierrc file as source
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "jsx-a11y/anchor-is-valid": [
-        "error",
-        {
-          components: ["Link"],
-          specialLink: ["hrefLeft", "hrefRight"],
-          aspects: ["invalidHref", "preferButton"],
-        },
-      ],
-    },
+    "react/react-in-jsx-scope": "off",
+    "jsx-a11y/anchor-is-valid": [
+      "error",
+      {
+        components: ["Link"],
+        specialLink: ["hrefLeft", "hrefRight"],
+        aspects: ["invalidHref", "preferButton"],
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
+  plugins: ["simple-import-sort"],
 };
