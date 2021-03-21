49418e684123d610001f28b9ec4e39ca61187f72
Timo Clasen <goebeltimo@gmail.com>
2021-03-21 15:57:04 +0100

    Add Padding and Spacing to Centerd Column Component

--- components/CenteredColumn.js
+++ components/CenteredColumn.js
@@ -1,3 +1,7 @@
 export default function CenteredColumn({ children }) {
-    return <section className="max-w-screen-xl mx-auto">{children}</section>;
+    return (
+        <section className="max-w-screen-xl px-6 mx-auto space-y-12">
+            {children}
+        </section>
+    );
 }
