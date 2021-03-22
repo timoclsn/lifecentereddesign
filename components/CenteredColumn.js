ef13dfebae36e94bdc00610f0e8e1b0b31832d91
Timo Clasen <goebeltimo@gmail.com>
2021-03-22 19:27:17 +0100

    Remove Vertical Spacing from Centered Column

--- components/CenteredColumn.js
+++ components/CenteredColumn.js
@@ -1,7 +1,5 @@
 export default function CenteredColumn({ children }) {
     return (
-        <section className="max-w-screen-xl px-6 mx-auto space-y-12">
-            {children}
-        </section>
+        <section className="max-w-screen-xl px-6 mx-auto">{children}</section>
     );
 }
