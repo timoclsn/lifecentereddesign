9a032daa784876c8c5a48134a5b8ecbc5e057ead
Timo Clasen <goebeltimo@gmail.com>
2021-03-27 21:00:54 +0100

    Remove Pointer Events

--- components/CO2Badge.js
+++ components/CO2Badge.js
@@ -12,8 +12,11 @@ export default function CO2Badge({ co2Consumption }) {
                 </span>
             </Popover.Trigger>
             <Popover.Content
-                className="max-w-xs px-6 py-10 space-y-6 sm:max-w-md bg-grass rounded-3xl"
+                className="max-w-xs px-6 py-10 space-y-6 pointer-events-none sm:max-w-md bg-grass rounded-3xl"
                 sideOffset={40}>
+                <Popover.Close>
+                    <FiX size={24} />
+                </Popover.Close>
                 <div className="space-y-6">
                     <h2 className="text-3xl font-bold">
                         Website carbon footprint:
