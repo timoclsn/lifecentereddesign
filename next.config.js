697736597ec42286872343ea19e1bf68156b84d0
Timo Clasen <goebeltimo@gmail.com>
2021-03-27 14:12:19 +0100

    Add Sitemap

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@ module.exports = {
     webpack: (config, { dev, isServer }) => {
         // Replace React with Preact only in client production build
         if (!dev && !isServer) {
+            if (isServer) {
+                require('./scripts/generate-sitemap');
+            }
+
             Object.assign(config.resolve.alias, {
                 react: 'preact/compat',
                 'react-dom/test-utils': 'preact/test-utils',
