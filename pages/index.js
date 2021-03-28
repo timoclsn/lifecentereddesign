113636a096ea59c66689241ce2691065a14dac98
Timo Clasen <goebeltimo@gmail.com>
2021-03-28 08:33:26 +0200

    Test Component Position

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,20 @@ import Header from '@/components/Header';
 import Layout from '@/components/Layout';
 import NewsletterCard from '@/components/NewsletterCard';
 import { getCO2Consumtion } from '@/lib/co2';
+import CO2Badge from '@/components/CO2Badge';
 
 export default function Home(props) {
     return (
-        <Layout co2Consumption={props.co2Consumption}>
-            <Header />
-            <Categories />
-            <About />
-            <NewsletterCard />
-            <AboutUs />
-        </Layout>
+        <>
+            <CO2Badge co2Consumption={props.co2Consumption} />
+            <Layout co2Consumption={props.co2Consumption}>
+                <Header />
+                <Categories />
+                <About />
+                <NewsletterCard />
+                <AboutUs />
+            </Layout>
+        </>
     );
 }
 
