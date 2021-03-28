2a4f3cebb8c6115e135584f2311740ef6d25d90b
Timo Clasen <goebeltimo@gmail.com>
2021-03-28 08:31:58 +0200

    Revert "Remove Popover Content"
    This reverts commit 37ff150f68a7ab96e22167c66d328773ffbf9dca.

--- components/CO2Badge.js
+++ components/CO2Badge.js
@@ -14,10 +14,42 @@ export default function CO2Badge({ co2Consumption }) {
             <Popover.Content
                 className="max-w-xs px-6 py-10 space-y-6 sm:max-w-md bg-grass rounded-3xl"
                 sideOffset={40}>
-                <h3>Popover content</h3>
-                <p>Are you sure you wanna do this?</p>
-                <Popover.Close>Yes</Popover.Close>
-                <Popover.Arrow />
+                <Popover.Close>
+                    <FiX size={24} />
+                </Popover.Close>
+                <div className="space-y-6">
+                    <h2 className="text-3xl font-bold">
+                        Website carbon footprint:
+                    </h2>
+                    <div className="space-y-4 text-dark">
+                        <p>
+                            Everytime someone opens this website only{' '}
+                            <strong>{co2Consumption.c} g of CO2</strong> are
+                            produced. This site is{' '}
+                            <strong>cleaner than {co2Consumption.p} %</strong>{' '}
+                            of web pages tested on Website Carbon Callculator.
+                        </p>
+                        <p>
+                            What we considered to make this page as clean as
+                            possible:
+                        </p>
+                        <ul className="list-disc list-inside">
+                            <li>
+                                Reduce information to text where possible – only
+                                use images when they bring a real value
+                            </li>
+                            <li>Static page</li>
+                        </ul>
+                    </div>
+                    <div className="flex space-x-4 text-dark">
+                        <FiArrowRight size={24} />
+                        <a
+                            href="https://www.websitecarbon.com"
+                            className="underline">
+                            Website Carbon Calculator
+                        </a>
+                    </div>
+                </div>
             </Popover.Content>
         </Popover.Root>
     );
