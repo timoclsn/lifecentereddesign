b2d705071aa1cee2be25ee2b62945232ba552618
Katharina Clasen <katharina.clasen@gmail.com>
2021-03-29 13:39:48 +0200

    Decrease Margin Bottom For Category Cards in Mobile

--- components/CategoryCard.js
+++ components/CategoryCard.js
@@ -1,7 +1,7 @@
 export default function CategoryCard({ headline, text, list, bgColor }) {
     return (
         <div
-            className={`inline-block w-full ${bgColor} rounded-3xl px-6 py-10 mb-14`}>
+            className={`inline-block w-full ${bgColor} rounded-3xl px-6 py-10 mb-8 sm:mb-14`}>
             <h3 className="mb-4 font-bold">{headline}</h3>
             <p className="text-dark">{text}</p>
             {list && (
