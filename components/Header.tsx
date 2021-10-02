1e1b78b6be17fe62a62a823d39c282ba7e7f3db6
Timo Clasen <goebeltimo@gmail.com>
2021-10-02 20:51:41 +0200

    Convert Header to Design System

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,25 @@
+import { Box } from './Box';
 import { Button } from './Button';
+import { Heading } from './Heading';
+import { Stack } from './Stack';
+import { Text } from './Text';
 
 export function Header() {
   return (
-    <section className="max-w-xl">
-      <h1 className="mb-6 text-3xl font-bold sm:text-5xl">
-        Here you will soon find a hub for life-centered design
-      </h1>
-      <p className="mb-10 text-dark">
-        This is supposed to be a home for information, news, resources, and
-        conversations around life-centered design to evolve beyond
-        human-centered and user experience design.
-      </p>
-      <Button
-        text="I want to get updates"
-        size="l"
-        href="http://eepurl.com/htoWRr"
-        target="_blank"
-      />
-    </section>
+    <Box as="section" maxWidth="640px">
+      <Stack space="32px">
+        <Heading>
+          Here you will soon find a hub for life-centered design
+        </Heading>
+        <Text as="p">
+          This is supposed to be a home for information, news, resources, and
+          conversations around life-centered design to evolve beyond
+          human-centered and user experience design.
+        </Text>
+        <Button as="a" href="http://eepurl.com/htoWRr" target="_blank">
+          I want to get updates
+        </Button>
+      </Stack>
+    </Box>
   );
 }
