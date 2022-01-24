618fd68200abc8bf1c99b6bd3a3a784ff8ae80f1
Timo Clasen <goebeltimo@gmail.com>
2022-01-24 20:24:31 +0100

    Update Dependencies
    Remove deprecated radix-ui/react-id.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,7 @@
 import '../styles/globals.css';
 
-import { IdProvider } from '@radix-ui/react-id';
-
 function MyApp({ Component, pageProps }) {
-  return (
-    <IdProvider>
-      <Component {...pageProps} />
-    </IdProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
