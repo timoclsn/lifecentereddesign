ed7b2828c29adf4e5a030c2e7e74100314d074d0
Timo Clasen <goebeltimo@gmail.com>
2022-09-04 23:19:58 +0200

    Remove Theme Color

--- components/Favicons.tsx
+++ components/Favicons.tsx
@@ -78,7 +78,6 @@ export function Favicons() {
         name="msapplication-TileImage"
         content="/favicons/ms-icon-144x144.png"
       />
-      <meta name="theme-color" content="#F0F2D1"></meta>
     </Head>
   );
 }
