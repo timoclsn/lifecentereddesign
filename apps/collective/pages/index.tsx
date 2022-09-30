038d679938f4c3480c497289b6c10af5f0e50515
Timo Clasen <goebeltimo@gmail.com>
2022-09-30 21:56:03 +0200

    Add Collective Page Package

---
+++ apps/collective/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from 'design-system';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <Button>Test</Button>
+    </div>
+  );
+};
+
+export default Home;
