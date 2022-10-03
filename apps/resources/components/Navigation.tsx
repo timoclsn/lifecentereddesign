54b812a5593ef7667667a9d68a3f4b13551665dc
Timo Clasen <goebeltimo@gmail.com>
2022-10-03 20:34:29 +0200

    Fix Navigation Hover State

--- apps/resources/components/Navigation.tsx
+++ apps/resources/components/Navigation.tsx
@@ -5,10 +5,10 @@ export function Navigation() {
   return (
     <header className="flex items-center justify-between py-6 px-6 sm:px-8 xl:px-10">
       <ul className="flex w-full items-baseline gap-10">
-        <li className="flex-1 font-serif text-2xl font-bold hover:underline">
+        <li className="flex-1 font-serif text-2xl font-bold">
           <Link href="/">
             <a>
-              <Heading as="span" level="4">
+              <Heading as="span" level="4" className="hover:underline">
                 Life Centered Design.Net
               </Heading>
             </a>
