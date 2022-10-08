264f57bbfc8369ddc29246ee29d21a78dff4e801
Timo Clasen <goebeltimo@gmail.com>
2022-10-08 15:45:32 +0200

    Update Syntax

--- packages/design-system/src/Container.tsx
+++ packages/design-system/src/Container.tsx
@@ -6,10 +6,10 @@ interface Props {
   inset?: boolean;
 }
 
-export function Container({ children, inset }: Props) {
+export const Container = ({ children, inset }: Props) => {
   const styles = cx([
     'mx-auto max-w-screen-2xl w-full',
     inset ? 'px-6 sm:px-8' : '',
   ]);
   return <div className={styles}>{children}</div>;
-}
+};
