aa6695af676debfbdb60acd8fb1344831714b408
Timo Clasen <goebeltimo@gmail.com>
2022-10-17 20:06:58 +0200

    Add New Resources Scroll Snap

--- apps/resources/components/NewResources.tsx
+++ apps/resources/components/NewResources.tsx
@@ -20,18 +20,21 @@ export const NewResources = ({ resources }: Props) => {
       <Heading level="2" className="text-white mb-10 px-6 sm:px-8 xl:px-10">
         New Resources
       </Heading>
-      <ul className="mb-14 flex gap-6 overflow-x-scroll px-6 sm:px-8 xl:px-10">
+      <ul className="mb-14 flex gap-6 overflow-x-scroll px-6 sm:px-8 xl:px-10 snap-x">
         <Image
           src="/ground.jpg"
           alt="Image of desert ground."
           width={320}
           height={400}
-          className="hidden rounded-4xl sm:block flex-none"
+          className="hidden rounded-4xl sm:block flex-none snap-center"
         />
         {newResources.map((resource) => {
           const component = getCardComponent(resource);
           return (
-            <li key={resource.id} className="w-[330px] flex-none sm:w-[600px]">
+            <li
+              key={resource.id}
+              className="w-[330px] flex-none sm:w-[600px] snap-center"
+            >
               {component}
             </li>
           );
