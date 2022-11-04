6ce79d2ed94d779787a174c136a7b23d851141c4
Timo Clasen <goebeltimo@gmail.com>
2022-11-04 13:34:40 +0100

    Use Moderen Image Formats

--- apps/collective/next.config.js
+++ apps/collective/next.config.js
@@ -3,6 +3,9 @@ const { withContentlayer } = require('next-contentlayer');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
   experimental: {
     scrollRestoration: true,
     transpilePackages: ['design-system'],
