5966401f4b8f2d0ae65da0b1a1b908d4ccac49d9
Timo Clasen <goebeltimo@gmail.com>
2022-11-12 14:06:42 +0100

    Update Dependencies

--- apps/collective/next.config.js
+++ apps/collective/next.config.js
@@ -3,10 +3,7 @@ const withTM = require('next-transpile-modules')(['design-system']);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   experimental: {
-    legacyBrowsers: false,
-    browsersListForSwc: true,
     scrollRestoration: true,
   },
 };
