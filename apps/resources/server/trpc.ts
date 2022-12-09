73b06134389ae9da544795274036a4503b550904
Timo Clasen <goebeltimo@gmail.com>
2022-12-09 20:53:26 +0100

    Add Performance Middleware

--- apps/resources/server/trpc.ts
+++ apps/resources/server/trpc.ts
@@ -5,5 +5,13 @@ const t = initTRPC.create({
   transformer: superjson,
 });
 
+const perfMiddleware = t.middleware(async ({ path, type, next }) => {
+  performance.mark("Start");
+  const result = await next();
+  performance.mark("End");
+  performance.measure(`[${result.ok ? "OK" : "ERROR"}][$1] ${type} '${path}'`, "Start", "End");
+  return result;
+});
+
 export const router = t.router;
-export const publicProcedure = t.procedure;
+export const publicProcedure = t.procedure.use(perfMiddleware);
