1e3a6f18e8306de9e15db11f83e6286fc04bf281
Timo Clasen <goebeltimo@gmail.com>
2023-02-15 22:54:31 +0100

    Form Improvements

--- apps/resources/components/Newsletter/Newsletter.tsx
+++ apps/resources/components/Newsletter/Newsletter.tsx
@@ -11,18 +11,19 @@ import {
   Container,
   Heading,
   InfoBox,
+  Link,
   Text,
 } from 'design-system';
 import { useZodForm } from 'hooks/useZodForm';
 import Image from 'next/image';
-import Link from 'next/link';
+import NextLink from 'next/link';
 import { SubmitHandler } from 'react-hook-form';
 import { trpc } from 'utils/trpc';
 import { z } from 'zod';
 import forestImg from './forest.jpg';
 
-const inputVariants = cva(
-  'px-10 py-6 text-2xl text-text-secondary bg-ghost-main-dark-bg focus-visible:outline-none md:w-[600px]',
+const inputStyles = cva(
+  'px-10 py-6 text-2xl text-text-secondary bg-ghost-main-dark-bg focus-visible:outline-none w-full',
   {
     variants: {
       error: {
@@ -72,7 +73,10 @@ export const Newsletter = () => {
 
   return (
     <Bleed>
-      <section className="relative overflow-hidden bg-[#EDF4EE]">
+      <section
+        id="newsletter"
+        className="relative overflow-hidden bg-[#EDF4EE]"
+      >
         <Image
           src={forestImg}
           alt="Image of a foggy forest."
@@ -88,66 +92,70 @@ export const Newsletter = () => {
               Life-centered Design related news delivered to your inbox once a
               month.
             </Text>
-            <form
-              className="w-full flex justify-center items-center gap-x-8 gap-y-12 flex-wrap mb-28"
-              onSubmit={handleSubmit(onSubmit)}
-            >
-              <div className="relative">
-                <label htmlFor="email" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  id="email"
-                  type="email"
-                  placeholder="Email address"
-                  className={inputVariants({ error: !!errors.email })}
-                  {...register('email')}
-                />
-                {errors.email && (
-                  <p className="absolute left-0 -bottom-8 text-red-700">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-              <Button type="submit" size="large">
-                {mutation.isLoading ? (
-                  <UilSpinnerAlt className="animate-spin" />
-                ) : (
-                  <UilEnvelopeAlt />
-                )}
-                Subscribe
-              </Button>
-            </form>
-            <Text as="p" size="small" className="mb-16 text-text-secondary">
-              By subscribing to our newsletter you also give us your consent
-              that we analyze, track and store the opening- and click-rates to
-              optimize our newsletter and services. You can unsubscribe at any
-              time by clicking the link in the footer of our emails. We use the
-              newsletter provider Mailchimp. For detailed information about our
-              privacy practices, please visit our{' '}
-              <Link href="/privacy" className="underline hover:opacity-80">
-                privacy policy
-              </Link>
-              .
-            </Text>
-            {(mutation.isError || mutation.isSuccess) && (
-              <div className="absolute left-0 bottom-10 w-full mt-20 animate-in slide-in-from-bottom-full duration-150 ease-in-out fade-in">
-                <Container inset className="flex flex-col items-center">
-                  {mutation.isError && (
-                    <InfoBox variant="error" icon={<UilExclamationTriangle />}>
-                      {mutation.error.message}
-                    </InfoBox>
+            <div className="max-w-4xl mx-auto relative">
+              <form
+                className="w-full flex justify-center items-center gap-x-8 gap-y-12 mb-10 flex-col sm:flex-row"
+                onSubmit={handleSubmit(onSubmit)}
+              >
+                <div className="relative flex-1 w-full">
+                  <label htmlFor="email" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email"
+                    type="email"
+                    placeholder="Email address"
+                    className={inputStyles({ error: !!errors.email })}
+                    {...register('email')}
+                  />
+                  {errors.email && (
+                    <p className="absolute left-0 -bottom-8 text-red-700">
+                      {errors.email.message}
+                    </p>
                   )}
-                  {mutation.isSuccess && (
-                    <InfoBox variant="success" icon={<UilCheckCircle />}>
-                      Almost finished... We need to confirm your email address.
-                      To complete the subscription process, please click the
-                      link in the email we just sent you.
-                    </InfoBox>
+                </div>
+                <Button type="submit" size="large">
+                  {mutation.isLoading ? (
+                    <UilSpinnerAlt className="animate-spin" />
+                  ) : (
+                    <UilEnvelopeAlt />
                   )}
-                </Container>
-              </div>
-            )}
+                  Subscribe
+                </Button>
+              </form>
+              {mutation.isSuccess && (
+                <InfoBox
+                  variant="success"
+                  icon={<UilCheckCircle />}
+                  className="animate-in zoom-in-0 duration-150 ease-in-out fade-in mb-6"
+                >
+                  Almost finished... We need to confirm your email address. To
+                  complete the subscription process, please click the link in
+                  the email we just sent you.
+                </InfoBox>
+              )}
+              {mutation.isError && (
+                <InfoBox
+                  variant="error"
+                  icon={<UilExclamationTriangle />}
+                  className="animate-in zoom-in-50 duration-150 ease-in-out fade-in mb-6"
+                >
+                  {mutation.error.message}
+                </InfoBox>
+              )}
+              <Text as="p" size="small" className="text-text-secondary mx-auto">
+                By subscribing to our newsletter you also give us your consent
+                that we analyze, track and store the opening- and click-rates to
+                optimize our newsletter and services. You can unsubscribe at any
+                time by clicking the link in the footer of our emails. We use
+                the newsletter provider Mailchimp. For detailed information
+                about our privacy practices, please visit our{' '}
+                <Link as={NextLink} url="/privacy">
+                  privacy policy
+                </Link>
+                .
+              </Text>
+            </div>
           </Container>
         </div>
       </section>
