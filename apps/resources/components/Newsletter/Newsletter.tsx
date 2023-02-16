b8e8769fa07c9eecd0c6a012f846905734871f20
Timo Clasen <goebeltimo@gmail.com>
2023-02-16 17:52:06 +0100

    Add Consens Checkbox

--- apps/resources/components/Newsletter/Newsletter.tsx
+++ apps/resources/components/Newsletter/Newsletter.tsx
@@ -1,9 +1,11 @@
 import {
+  UilCheck,
   UilCheckCircle,
   UilEnvelopeAlt,
   UilExclamationTriangle,
   UilSpinnerAlt,
 } from '@iconscout/react-unicons';
+import * as Checkbox from '@radix-ui/react-checkbox';
 import { cva } from 'class-variance-authority';
 import {
   Bleed,
@@ -17,13 +19,25 @@ import {
 import { useZodForm } from 'hooks/useZodForm';
 import Image from 'next/image';
 import NextLink from 'next/link';
-import { SubmitHandler } from 'react-hook-form';
+import { Controller, SubmitHandler } from 'react-hook-form';
 import { trpc } from 'utils/trpc';
 import { z } from 'zod';
 import forestImg from './forest.jpg';
 
 const inputStyles = cva(
-  'px-10 py-6 text-2xl text-text-secondary bg-ghost-main-dark-bg focus-visible:outline-none w-full',
+  'px-10 py-6 text-2xl text-text-secondary bg-ghost-main-dark-bg outline-none w-full ring-inset',
+  {
+    variants: {
+      error: {
+        true: 'ring-2 ring-red-700',
+        false: 'focus-visible:ring-2 focus-visible:ring-ghost-contrast-text',
+      },
+    },
+  }
+);
+
+const checkboxStyles = cva(
+  'flex h-[25px] w-[25px] items-center justify-center bg-ghost-main-dark-bg outline-none ring-inset flex-none',
   {
     variants: {
       error: {
@@ -39,6 +53,11 @@ export const newsletterFormSchema = z.object({
   email: z.string().min(1, { message: 'Email address is required' }).email({
     message: 'Must be a valid email address',
   }),
+  consens: z.literal(true, {
+    errorMap: () => ({
+      message: 'You must confirm that you want to subscribe',
+    }),
+  }),
 });
 
 export const Newsletter = () => {
@@ -48,6 +67,7 @@ export const Newsletter = () => {
     formState: { errors },
     reset,
     setFocus,
+    control,
   } = useZodForm({
     schema: newsletterFormSchema,
   });
@@ -65,9 +85,13 @@ export const Newsletter = () => {
     },
   });
 
-  const onSubmit: SubmitHandler<NewsletterFormSchema> = ({ email }) => {
+  const onSubmit: SubmitHandler<NewsletterFormSchema> = ({
+    email,
+    consens,
+  }) => {
     mutation.mutate({
       email,
+      consens,
     });
   };
 
@@ -84,7 +108,7 @@ export const Newsletter = () => {
         />
         <div className="relative py-28">
           <Container inset>
-            <Heading level="2" className="text-primary mb-10">
+            <Heading level="2" className="text-primary">
               Newsletter
             </Heading>
             <Text as="p" size="large" className="mb-16 text-text-secondary">
@@ -92,42 +116,77 @@ export const Newsletter = () => {
               Life-centered Design related news delivered to your inbox once a
               month.
             </Text>
-            <div className="max-w-4xl mx-auto relative">
-              <form
-                className="w-full flex justify-center items-center gap-x-8 gap-y-12 mb-10 flex-col sm:flex-row"
-                onSubmit={handleSubmit(onSubmit)}
-              >
-                <div className="relative flex-1 w-full">
-                  <label htmlFor="email" className="sr-only">
-                    Email address
-                  </label>
-                  <input
-                    id="email"
-                    type="email"
-                    placeholder="Email address"
-                    className={inputStyles({ error: !!errors.email })}
-                    {...register('email')}
-                  />
-                  {errors.email && (
-                    <p className="absolute left-0 -bottom-8 text-red-700">
-                      {errors.email.message}
-                    </p>
+
+            {/* Form */}
+            <form
+              className="w-full max-w-4xl mx-auto flex flex-col items-start gap-10"
+              onSubmit={handleSubmit(onSubmit)}
+            >
+              {/* Email input */}
+              <div className="relative w-full">
+                <label htmlFor="email" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email"
+                  type="email"
+                  placeholder="Email address"
+                  className={inputStyles({ error: !!errors.email })}
+                  {...register('email')}
+                />
+                {errors.email && (
+                  <p className="absolute left-0 bottom-0 -mb-6 text-red-700 text-sm">
+                    {errors.email.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Consens checkbox */}
+              <div className="relative flex gap-3 items-center">
+                <Controller
+                  name="consens"
+                  control={control}
+                  render={({ field }) => (
+                    <Checkbox.Root
+                      {...field}
+                      id="consens"
+                      value={undefined}
+                      checked={field.value}
+                      onCheckedChange={field.onChange}
+                      className={checkboxStyles({ error: !!errors.consens })}
+                    >
+                      <Checkbox.Indicator className="animate-in zoom-in-150 fade-in-50 duration-100">
+                        <UilCheck />
+                      </Checkbox.Indicator>
+                    </Checkbox.Root>
                   )}
-                </div>
-                <Button type="submit" size="large">
-                  {mutation.isLoading ? (
-                    <UilSpinnerAlt className="animate-spin" />
-                  ) : (
-                    <UilEnvelopeAlt />
-                  )}
-                  Subscribe
-                </Button>
-              </form>
+                />
+                <label className="Label" htmlFor="consens">
+                  Yes, I want to receive the newsletter
+                </label>
+                {errors.consens && (
+                  <p className="absolute left-0 bottom-0 -mb-6 text-red-700 text-sm">
+                    {errors.consens.message}
+                  </p>
+                )}
+              </div>
+
+              {/* Submit button */}
+              <Button type="submit" size="large">
+                {mutation.isLoading ? (
+                  <UilSpinnerAlt className="animate-spin" />
+                ) : (
+                  <UilEnvelopeAlt />
+                )}
+                Subscribe
+              </Button>
+
+              {/* Server error */}
               {mutation.isSuccess && (
                 <InfoBox
                   variant="success"
                   icon={<UilCheckCircle />}
-                  className="animate-in zoom-in-0 duration-150 ease-in-out fade-in mb-8"
+                  className="animate-in zoom-in-0 duration-150 ease-in-out fade-in"
                 >
                   Almost finished... We need to confirm your email address. To
                   complete the subscription process, please click the link in
@@ -138,11 +197,13 @@ export const Newsletter = () => {
                 <InfoBox
                   variant="error"
                   icon={<UilExclamationTriangle />}
-                  className="animate-in zoom-in-50 duration-150 ease-in-out fade-in mb-8"
+                  className="animate-in zoom-in-50 duration-150 ease-in-out fade-in"
                 >
                   {mutation.error.message}
                 </InfoBox>
               )}
+
+              {/* Info text */}
               <Text as="p" size="small" className="text-text-secondary mx-auto">
                 By subscribing to our newsletter you also give us your consent
                 that we analyze, track and store the opening- and click-rates to
@@ -153,9 +214,12 @@ export const Newsletter = () => {
                 <Link as={NextLink} url="/privacy">
                   privacy policy
                 </Link>
-                .
+                . Learn more about Mailchimp&apos;s privacy practices{' '}
+                <Link url="https://mailchimp.com/legal/terms" external>
+                  here.
+                </Link>
               </Text>
-            </div>
+            </form>
           </Container>
         </div>
       </section>
--- apps/resources/server/routers/newsletter.ts
+++ apps/resources/server/routers/newsletter.ts
@@ -1,3 +1,4 @@
+import mailchimp from '@mailchimp/mailchimp_marketing';
 import { TRPCError } from '@trpc/server';
 import { newsletterFormSchema } from 'components/Newsletter/Newsletter';
 import { publicProcedure, router } from 'server/trpc';
@@ -7,40 +8,42 @@ const envSchema = z.object({
   MAILCHIMP_API_KEY: z.string(),
   MAILCHIMP_API_SERVER: z.string(),
   MAILCHIMP_AUDIENCE_ID: z.string(),
+  MAILCHIMP_MARKETING_PERMISSION_ID: z.string(),
 });
 
 const {
   MAILCHIMP_API_KEY: apiKey,
   MAILCHIMP_API_SERVER: apiServer,
   MAILCHIMP_AUDIENCE_ID: audienceId,
+  MAILCHIMP_MARKETING_PERMISSION_ID: marketingPermissionId,
 } = envSchema.parse(process.env);
 
-const url = `https://${apiServer}.api.mailchimp.com/3.0/lists/${audienceId}/members`;
+mailchimp.setConfig({
+  apiKey: apiKey,
+  server: apiServer,
+});
 
 export const newsletterRouter = router({
   subscribe: publicProcedure
     .input(newsletterFormSchema)
     .mutation(async ({ input }) => {
-      const { email } = input;
+      const { email, consens } = input;
 
-      const data = {
-        email_address: email,
-        status: 'pending',
-      };
-
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `api_key ${apiKey}`,
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (!response.ok) {
-        console.log(response);
+      try {
+        await mailchimp.lists.addListMember(audienceId, {
+          email_address: email,
+          status: 'pending',
+          marketing_permissions: [
+            {
+              marketing_permission_id: marketingPermissionId,
+              enabled: consens,
+            },
+          ],
+        });
+      } catch (e) {
+        console.log(e);
         throw new TRPCError({
-          code: 'INTERNAL_SERVER_ERROR',
+          code: 'BAD_REQUEST',
           message:
             "There was an error subscribing to the newsletter. Send me an email at katharina@katharinaclasen.de and I'll add you to the list.",
         });
