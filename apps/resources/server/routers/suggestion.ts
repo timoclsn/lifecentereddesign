63f58fdc2093f9968f61055effbf6be33259aa85
Timo Clasen <goebeltimo@gmail.com>
2023-02-20 21:13:21 +0100

    Update Vars

--- apps/resources/server/routers/suggestion.ts
+++ apps/resources/server/routers/suggestion.ts
@@ -4,18 +4,10 @@ import { publicProcedure, router } from 'server/trpc';
 import { z } from 'zod';
 
 const envSchema = z.object({
-  SUGGESTION_MAIL_HOST: z.string(),
-  SUGGESTION_MAIL_PORT: z.coerce.number(),
-  SUGGESTION_MAIL_USER: z.string(),
   SUGGESTION_MAIL_PASSWORD: z.string(),
 });
 
-const {
-  SUGGESTION_MAIL_HOST,
-  SUGGESTION_MAIL_PORT,
-  SUGGESTION_MAIL_USER,
-  SUGGESTION_MAIL_PASSWORD,
-} = envSchema.parse(process.env);
+const { SUGGESTION_MAIL_PASSWORD } = envSchema.parse(process.env);
 
 export const suggestionRouter = router({
   add: publicProcedure
@@ -29,10 +21,10 @@ export const suggestionRouter = router({
       const { link, message } = input;
 
       const transporter = nodemailer.createTransport({
-        port: SUGGESTION_MAIL_PORT,
-        host: SUGGESTION_MAIL_HOST,
+        port: 465,
+        host: 'sslout.de',
         auth: {
-          user: SUGGESTION_MAIL_USER,
+          user: 'suggestion@lifecentereddesign.net',
           pass: SUGGESTION_MAIL_PASSWORD,
         },
         secure: true,
