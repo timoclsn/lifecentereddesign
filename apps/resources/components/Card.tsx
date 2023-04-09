e615579f760742443597eb8902838e850bcdaf6b
Timo Clasen <goebeltimo@gmail.com>
2023-04-10 00:07:22 +0200

    Revert "Dont invalidate on Settled"
    This reverts commit 233d6b2d55cb1bfb8f5ad2b4a49bf9344d16c68b.

--- apps/resources/components/Card.tsx
+++ apps/resources/components/Card.tsx
@@ -76,7 +76,6 @@ export const Card = ({
 
   const likeMutation = trpc.resources.like.useMutation({
     onMutate: () => {
-      console.log('like');
       utils.resources.likes.cancel(input);
 
       const oldData = utils.resources.likes.getData(input);
@@ -99,11 +98,13 @@ export const Card = ({
     onError: (err, input, context) => {
       utils.resources.likes.setData(input, context?.oldData);
     },
+    onSettled: () => {
+      utils.resources.likes.invalidate(input);
+    },
   });
 
   const unlikeMutation = trpc.resources.unlike.useMutation({
     onMutate: () => {
-      console.log('unlike');
       utils.resources.likes.cancel(input);
 
       const oldData = utils.resources.likes.getData(input);
@@ -125,6 +126,9 @@ export const Card = ({
     },
     onError: (err, input, context) => {
       utils.resources.likes.setData(input, context?.oldData);
+    },
+    onSettled: () => {
+      utils.resources.likes.invalidate(input);
     },
   });
 
