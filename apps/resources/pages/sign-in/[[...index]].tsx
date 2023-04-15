17080167d8a317c224b481478fc4269e01c02fa4
Timo Clasen <goebeltimo@gmail.com>
2023-04-15 22:30:10 +0200

    Update Auth Pages

--- apps/resources/pages/sign-in/[[...index]].tsx
+++ apps/resources/pages/sign-in/[[...index]].tsx
@@ -1,24 +1,30 @@
 import { SignIn } from '@clerk/nextjs';
 import { Layout } from 'components/Layout';
+import { Heading } from 'design-system';
 
 const SignInPage = () => (
   <Layout title="Sign in" slug="sign-in">
-    <section className="flex items-center justify-center">
-      <SignIn
-        path="/sign-in"
-        routing="path"
-        signUpUrl="/sign-up"
-        appearance={{
-          layout: {
-            privacyPageUrl: '/privacy',
-            socialButtonsPlacement: 'bottom',
-            socialButtonsVariant: 'blockButton',
-          },
-          variables: {
-            colorPrimary: '#101b2c',
-          },
-        }}
-      />
+    <section>
+      <Heading level="1" className="max-w-3x mb-8">
+        Sign in
+      </Heading>
+      <div className="flex items-center justify-center">
+        <SignIn
+          path="/sign-in"
+          routing="path"
+          signUpUrl="/sign-up"
+          appearance={{
+            layout: {
+              privacyPageUrl: '/privacy',
+              socialButtonsPlacement: 'bottom',
+              socialButtonsVariant: 'blockButton',
+            },
+            variables: {
+              colorPrimary: '#101b2c',
+            },
+          }}
+        />
+      </div>
     </section>
   </Layout>
 );
--- apps/resources/pages/sign-up/[[...index]].tsx
+++ apps/resources/pages/sign-up/[[...index]].tsx
@@ -1,24 +1,42 @@
 import { SignUp } from '@clerk/nextjs';
 import { Layout } from 'components/Layout';
+import { Heading, Link, Text } from 'design-system';
 
 const SignUpPage = () => (
   <Layout title="Sign up" slug="sign-up">
-    <section className="flex items-center justify-center">
-      <SignUp
-        path="/sign-up"
-        routing="path"
-        signInUrl="/sign-in"
-        appearance={{
-          layout: {
-            privacyPageUrl: '/privacy',
-            socialButtonsPlacement: 'bottom',
-            socialButtonsVariant: 'blockButton',
-          },
-          variables: {
-            colorPrimary: '#101b2c',
-          },
-        }}
-      />
+    <section>
+      <Heading level="1" className="max-w-3x mb-8">
+        Sign up
+      </Heading>
+      <Text as="p" size="large" className="text-text-secondary mb-20 max-w-5xl">
+        Sign up for an account to be able to use personalized features like
+        liking resources…
+      </Text>
+      <div className="flex flex-col items-center justify-center gap-20">
+        <SignUp
+          path="/sign-up"
+          routing="path"
+          signInUrl="/sign-in"
+          appearance={{
+            layout: {
+              privacyPageUrl: '/privacy',
+              socialButtonsPlacement: 'bottom',
+              socialButtonsVariant: 'blockButton',
+            },
+            variables: {
+              colorPrimary: '#101b2c',
+            },
+          }}
+        />
+        <Text
+          as="p"
+          size="medium"
+          className="text-text-secondary mb-20 max-w-md"
+        >
+          With your registration you declare to have read and to agree with our{' '}
+          <Link url="/privacy">privacy policy</Link>.
+        </Text>
+      </div>
     </section>
   </Layout>
 );
