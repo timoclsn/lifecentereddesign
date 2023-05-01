9e72b7328d02b93da662d4891dbfa1087deaca8a
Timo Clasen <goebeltimo@gmail.com>
2023-05-01 22:07:30 +0200

    Update Dependencies (#206)
    * Update Dependencies
    
    * Enable Prisma JSON Protocol
    
    * Fix Icon
    
    * Downgrade Contentlayer
    
    * Revert "Downgrade Contentlayer"
    
    This reverts commit b48563ea23dafb8c51223984cb968810544cbe0b.
    
    * Test

--- apps/resources/components/Icons/SolidHeart.tsx
+++ apps/resources/components/Icons/SolidHeart.tsx
@@ -9,7 +9,7 @@ export const SolidHeart = ({ className, size = 24 }: Props) => {
       fill="currentColor"
       height={size}
       width={size}
-      viewBox={`0 0 ${size} ${size}}`}
+      viewBox={`0 0 ${size} ${size}`}
       className={className}
       xmlns="http://www.w3.org/2000/svg"
     >
