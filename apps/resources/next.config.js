a45beb713d3dde7147178badc00a143c1cbc684e
Timo Clasen <goebeltimo@gmail.com>
2023-05-05 21:02:20 +0200

    Update Dependencies (#207)

--- apps/resources/next.config.js
+++ apps/resources/next.config.js
@@ -8,7 +8,6 @@ const config = {
   },
   transpilePackages: ['design-system'],
   experimental: {
-    scrollRestoration: true,
     swcPlugins: [['next-superjson-plugin', {}]],
   },
   async rewrites() {
