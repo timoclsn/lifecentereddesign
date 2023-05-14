14bb87ba05d15aa36f06b57c2b4053f6cc1321e1
Timo Clasen <goebeltimo@gmail.com>
2023-05-14 21:56:47 +0200

    Open Tooltip On Click

--- packages/design-system/src/Tooltip.tsx
+++ packages/design-system/src/Tooltip.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
 import * as TooltipPrimitive from '@radix-ui/react-tooltip';
 
 interface Props {
@@ -8,10 +8,20 @@ interface Props {
 }
 
 export const Tooltip = ({ children, content, delayDuration = 0 }: Props) => {
+  const [open, setOpen] = useState(false);
   return (
     <TooltipPrimitive.Provider>
-      <TooltipPrimitive.Root delayDuration={delayDuration}>
-        <TooltipPrimitive.Trigger asChild>{children}</TooltipPrimitive.Trigger>
+      <TooltipPrimitive.Root
+        delayDuration={delayDuration}
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <TooltipPrimitive.Trigger
+          asChild
+          onClick={() => setOpen((value) => !value)}
+        >
+          {children}
+        </TooltipPrimitive.Trigger>
         <TooltipPrimitive.Portal>
           <TooltipPrimitive.Content
             className="bg-primary-main-bg text-primary-contrast-text animate-in fade-in data-[state=delayed-open]:data-[side=top]:slide-in-from-bottom-[20px] data-[state=delayed-open]:data-[side=top]:zoom-in-75 z-50 max-w-xs rounded-md p-4 text-sm duration-150"
