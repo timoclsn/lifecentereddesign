7846b025ef41edb3651cc4b6d081b1f04fd6514b
Timo Clasen <goebeltimo@gmail.com>
2023-07-23 20:56:53 +0200

    Migrate Suggestion Form

---
+++ apps/resources/app/resources/page.tsx
@@ -0,0 +1,11 @@
+import { Suggestion } from 'components/Suggestion/Suggestion';
+
+const ResourcesPage = () => {
+  return (
+    <>
+      <Suggestion />
+    </>
+  );
+};
+
+export default ResourcesPage;
---
+++ apps/resources/components/Suggestion/actions.ts
@@ -0,0 +1,47 @@
+'use server';
+
+import nodemailer from 'nodemailer';
+import {
+  SuggestionFormSchema,
+  envSchema,
+  suggestionFormSchema,
+} from './schemas';
+
+const { SUGGESTION_MAIL_PASSWORD } = envSchema.parse(process.env);
+
+export const submit = async (input: SuggestionFormSchema) => {
+  const { link, message, name } = suggestionFormSchema.parse(input);
+
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'sslout.de',
+    auth: {
+      user: 'suggestion@lifecentereddesign.net',
+      pass: SUGGESTION_MAIL_PASSWORD,
+    },
+    secure: true,
+  });
+
+  const mailData = {
+    from: 'suggestion@lifecentereddesign.net',
+    to: 'hello@lifecentereddesign.net',
+    subject: 'Resource Suggestion',
+    text: `Link: ${link}\nMessage: ${
+      message ? message : 'No message provided'
+    }\nName: ${name ? name : 'No name provided'}`,
+  };
+
+  try {
+    await transporter.sendMail(mailData);
+  } catch (e) {
+    console.error(e);
+    return {
+      error:
+        'There was an error submitting your suggestion. Please try again or send it via email at hello@lifecentereddesign.net.',
+    };
+  }
+
+  return {
+    error: '',
+  };
+};
