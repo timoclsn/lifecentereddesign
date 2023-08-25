8cfb4801ac5f8889ef81f00f883827db977f57d5
Timo Clasen <goebeltimo@gmail.com>
2023-08-25 21:31:13 +0200

    Update Component

--- apps/resources/components/RefreshOnNavigation/RefreshOnNavigation.tsx
+++ apps/resources/components/RefreshOnNavigation/RefreshOnNavigation.tsx
@@ -3,23 +3,13 @@
 import { usePathname, useRouter } from 'next/navigation';
 import { useEffect } from 'react';
 
-interface Props {
-  timeout?: number;
-}
-
-export const RefreshOnNavigation = ({ timeout = 0 }: Props) => {
+export const RefreshOnNavigation = () => {
   const { refresh } = useRouter();
   const pathname = usePathname();
 
   useEffect(() => {
-    const timeoutId = setTimeout(() => {
-      refresh();
-    }, timeout);
-
-    return () => {
-      clearTimeout(timeoutId);
-    };
-  }, [pathname, refresh, timeout]);
+    refresh();
+  }, [pathname, refresh]);
 
   return null;
 };
