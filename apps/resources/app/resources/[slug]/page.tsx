ed75cf08afc3be11f4bded3f77220858e03b25e5
Timo Clasen <goebeltimo@gmail.com>
2023-08-29 21:09:58 +0200

    Revert "Make it build"
    This reverts commit bc56e99b011049d6f554095d0b7518bf59372c54.

--- apps/resources/app/resources/[slug]/page.tsx
+++ apps/resources/app/resources/[slug]/page.tsx
@@ -59,7 +59,6 @@ interface Props {
 
 const ResourcePage = async ({ params }: Props) => {
   const { slug } = params;
-  console.log(slug);
   const { resourceId, resourceType } = parseResourceSlug(slug);
 
   return (
