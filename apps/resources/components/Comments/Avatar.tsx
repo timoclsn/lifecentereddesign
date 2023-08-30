de3e6212769ad839a62361ca58734fa5a4436c1c
Timo Clasen <goebeltimo@gmail.com>
2023-08-30 22:10:16 +0200

    Update Avatar Fallback

--- apps/resources/components/Comments/Avatar.tsx
+++ apps/resources/components/Comments/Avatar.tsx
@@ -9,14 +9,19 @@ export const Avatar = ({ url, username }: Props) => {
   return (
     <div className="relative h-6 w-6">
       <div className="bg-stone absolute left-0 top-0 h-full w-full animate-pulse rounded-full" />
-      {url && (
-        <Image
-          src={url}
-          alt={`Avatar image of ${username}`}
-          width={24}
-          height={24}
-          className="relative block h-full w-full rounded-full"
-        />
+      {url ? (
+        <>
+          <div className="bg-stone absolute left-0 top-0 h-full w-full animate-pulse rounded-full" />
+          <Image
+            src={url}
+            alt={`Avatar image of ${username}`}
+            width={24}
+            height={24}
+            className="relative block h-full w-full rounded-full"
+          />
+        </>
+      ) : (
+        <div className="bg-primary-main-bg absolute left-0 top-0 h-full w-full rounded-full" />
       )}
     </div>
   );
