f846eb0cfe7e96152b1d066e668a65841e7a773c
Timo Clasen <goebeltimo@gmail.com>
2023-08-31 22:11:04 +0200

    Refactor Create serverAction (#235)
    * Refactor Create Server Action
    
    * Move Auth Call

--- apps/resources/app/profile/actions.ts
+++ apps/resources/app/profile/actions.ts
@@ -1,24 +1,26 @@
 'use server';
 
-import { auth, clerkClient } from '@clerk/nextjs';
+import { clerkClient } from '@clerk/nextjs';
 import { revalidatePath } from 'next/cache';
 import { createAction } from '../../lib/actions/createAction';
 import { deleteUserData } from '../../lib/resources';
 
-export const deleteAccount = createAction()(async () => {
-  const { userId } = auth();
+export const deleteAccount = createAction({
+  action: async ({ ctx }) => {
+    const { userId } = ctx;
 
-  if (!userId) {
-    throw new Error('You must be logged in to delete your account.');
-  }
+    if (!userId) {
+      throw new Error('You must be logged in to delete your account.');
+    }
 
-  try {
-    await clerkClient.users.deleteUser(userId);
-    await deleteUserData(userId);
-    revalidatePath('/');
-  } catch (error) {
-    throw new Error(
-      'Something went wrong while deleting your account. Please try again or contact us at hello@lifecentereddesign.net.',
-    );
-  }
+    try {
+      await clerkClient.users.deleteUser(userId);
+      await deleteUserData(userId);
+      revalidatePath('/');
+    } catch (error) {
+      throw new Error(
+        'Something went wrong while deleting your account. Please try again or contact us at hello@lifecentereddesign.net.',
+      );
+    }
+  },
 });
--- apps/resources/app/resources/Suggestion/actions.ts
+++ apps/resources/app/resources/Suggestion/actions.ts
@@ -6,36 +6,37 @@ import { envSchema, suggestionFormSchema } from './schemas';
 
 const { SUGGESTION_MAIL_PASSWORD } = envSchema.parse(process.env);
 
-export const submit = createAction(suggestionFormSchema)(async ({
-  link,
-  message,
-  name,
-}) => {
-  const transporter = nodemailer.createTransport({
-    port: 465,
-    host: 'sslout.de',
-    auth: {
-      user: 'suggestion@lifecentereddesign.net',
-      pass: SUGGESTION_MAIL_PASSWORD,
-    },
-    secure: true,
-  });
+export const submit = createAction({
+  input: suggestionFormSchema,
+  action: async ({ input }) => {
+    const { link, message, name } = input;
 
-  const mailData = {
-    from: 'suggestion@lifecentereddesign.net',
-    to: 'hello@lifecentereddesign.net',
-    subject: 'Resource Suggestion',
-    text: `Link: ${link}\nMessage: ${
-      message ? message : 'No message provided'
-    }\nName: ${name ? name : 'No name provided'}`,
-  };
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: 'sslout.de',
+      auth: {
+        user: 'suggestion@lifecentereddesign.net',
+        pass: SUGGESTION_MAIL_PASSWORD,
+      },
+      secure: true,
+    });
 
-  try {
-    await transporter.sendMail(mailData);
-  } catch (e) {
-    console.error(e);
-    throw new Error(
-      'There was an error submitting your suggestion. Please try again or send it via email at hello@lifecentereddesign.net.',
-    );
-  }
+    const mailData = {
+      from: 'suggestion@lifecentereddesign.net',
+      to: 'hello@lifecentereddesign.net',
+      subject: 'Resource Suggestion',
+      text: `Link: ${link}\nMessage: ${
+        message ? message : 'No message provided'
+      }\nName: ${name ? name : 'No name provided'}`,
+    };
+
+    try {
+      await transporter.sendMail(mailData);
+    } catch (e) {
+      console.error(e);
+      throw new Error(
+        'There was an error submitting your suggestion. Please try again or send it via email at hello@lifecentereddesign.net.',
+      );
+    }
+  },
 });
