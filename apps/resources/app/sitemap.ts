897b5a339781be19e91b3cf4b000c102fd074926
Timo Clasen <goebeltimo@gmail.com>
2023-10-28 21:40:55 +0200

    Add Collections to Sitemap

--- apps/resources/app/sitemap.ts
+++ apps/resources/app/sitemap.ts
@@ -1,5 +1,7 @@
 import { MetadataRoute } from 'next';
 import { getResources } from '../lib/resources';
+import { featureFlags } from 'lib/featureFlags';
+import { getCollections } from 'lib/collections';
 
 export const dynamic = 'force-dynamic';
 
@@ -39,6 +41,24 @@ const sitemap = async (): Promise<MetadataRoute.Sitemap> => {
       lastModified: new Date(),
     });
   });
+
+  const flags = await featureFlags();
+
+  if (flags.collections) {
+    pages.push({
+      url: 'https://lifecentereddesign.net/collections',
+      lastModified: new Date(),
+    });
+
+    const collections = await getCollections();
+
+    collections.forEach((collection) => {
+      pages.push({
+        url: `https://lifecentereddesign.net/collections/${collection.id}`,
+        lastModified: new Date(),
+      });
+    });
+  }
 
   return pages;
 };
