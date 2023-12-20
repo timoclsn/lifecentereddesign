d08e4f8ecac8844a3059731e7c622fc78a5466b8
Timo Clasen <goebeltimo@gmail.com>
2023-12-20 22:29:22 +0100

    Remove console.log statements

--- apps/resources/components/Await/Await.tsx
+++ apps/resources/components/Await/Await.tsx
@@ -32,7 +32,6 @@ const InnerAwait = async <T extends unknown>({
   promise,
   children,
 }: InnerAwaitProps<T>) => {
-  console.log({ promise });
   const data = await promise;
   console.log({ data });
   return children(data);
