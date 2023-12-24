c145026fd4e8880d7fb05c482bf69d7b22486cff
Timo Clasen <goebeltimo@gmail.com>
2023-12-24 22:38:30 +0100

    Remove DB Adapter (#270)

--- packages/database/prisma.ts
+++ packages/database/prisma.ts
@@ -1,20 +1,13 @@
-import { connect } from '@planetscale/database';
-import { PrismaPlanetScale } from '@prisma/adapter-planetscale';
 import { PrismaClient } from '@prisma/client';
 import { z } from 'zod';
 
 const env = z.string().parse(process.env.NODE_ENV);
-const connectionString = z.string().parse(process.env.DATABASE_URL);
-
-const connection = connect({ url: connectionString });
-const adapter = new PrismaPlanetScale(connection);
 
 const prismaGlobal = global as typeof global & {
   prisma?: PrismaClient;
 };
 
-export const prisma: PrismaClient =
-  prismaGlobal.prisma || new PrismaClient({ adapter });
+export const prisma: PrismaClient = prismaGlobal.prisma || new PrismaClient();
 
 if (env !== 'production') {
   prismaGlobal.prisma = prisma;
